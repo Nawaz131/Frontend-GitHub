5f84eb48d21b7d26d78b38b87aa26c0b253fc4fe
Nawaz Hussain <nawazbangalore@example.com>
2026-07-19 00:31:00 +0530

    Connect frontend with Render backend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,61 @@
+
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link to="/">
-        <div>
+    <nav className="github-navbar">
+      <div className="navbar-left">
+        <button className="nav-square-btn">☰</button>
+
+        <Link to="/">
           <img
+            className="github-logo"
             src="https://www.github.com/images/modules/logos_page/GitHub-Mark.png"
             alt="GitHub logo"
           />
-          <h3>GitHub</h3>
-        </div>
-      </Link>
-      <div>
-        <Link to="/create">
-          <p>Create a Repository</p>
         </Link>
+
+        <Link to="/" className="dashboard-link">
+          Dashboard
+        </Link>
+      </div>
+
+      <div className="navbar-right">
+        <div className="search-box">
+          <span>
+            <i class="bi bi-search"></i>
+          </span>
+          <input type="text" placeholder="Type / to search" />
+        </div>
+
+        <Link to="/create">
+          <button className="nav-square-btn">
+            <i class="bi bi-plus-square"></i>
+          </button>
+        </Link>
+
+        <button className="nav-square-btn">
+          <i class="bi bi-record-circle"></i>
+        </button>
+
+        <button className="nav-square-btn">
+          <i className="bi bi-git"></i>
+        </button>
+
+        <button className="nav-square-btn">
+          <i class="bi bi-journal-text"></i>
+        </button>
+
+        <button className="nav-square-btn">
+          <i class="bi bi-inbox"></i>
+        </button>
+
         <Link to="/profile">
-          <p>Profile</p>
+          <div className="profile-avatar">
+            <i class="bi bi-person-circle"></i>
+          </div>
         </Link>
       </div>
     </nav>
